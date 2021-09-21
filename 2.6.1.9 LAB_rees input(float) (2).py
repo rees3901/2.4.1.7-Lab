89c37cbe5b87530366af6154c2c7057fd652a517
rees3901 <73313088+rees3901@users.noreply.github.com>
2021-09-21 11:14:00 +0100

    Add files via upload

---
+++ 2.6.1.9 LAB_rees input(float) (2).py
@@ -0,0 +1,6 @@
+﻿a = float(input("Enter first value: "))
+b = float(input("Enter second value: "))
+print("Addition:, a + b \n Subtraction:, a - b \n Multiplication:, a * b \n")
+print()
+
+print("\nThat's all, folks!")
