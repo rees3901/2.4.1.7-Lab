012872190074640bda65b62c025f3796a138a416
rees3901 <73313088+rees3901@users.noreply.github.com>
2021-09-21 11:59:44 +0100

    Add files via upload

---
+++ Liam rees 2.6.1.10 LAB float input.py
@@ -0,0 +1,7 @@
+﻿#Liam rees 2.6.1.10 LAB float input
+
+x = float(input("Enter value for x: "))
+
+y = 1.0 /(x+1/(x+1/ x+1/x)) #  this is the maths part that i stuggled to representWrite your code here.
+
+print("y =", y)
